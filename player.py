ed2a9509ed5f4a6319449c0b5d1d31457ca9288d
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-04-04 19:04:26 -0300

    after com recursão

--- player.py
+++ player.py
@@ -3,8 +3,6 @@ import pygame
 import customtkinter
 import tkinter
 import os
-import threading
-import time
 
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("green")
@@ -28,26 +26,50 @@ def play_sound():
     music = get_music()
     if not music: return # TODO: "selecione uma musica"
     
+    global is_pause
+    is_pause = False
+    
     print(music)
     sound_mutagen = MP3(music)
     sound_duration = sound_mutagen.info.length
     pygame.mixer.music.load(music)
     pygame.mixer.music.play()
 
-    progress.set(0)
-    for t in steps(sound_duration):
-        time.sleep(0.01)
-        progress.set(t)
-        app.update_idletasks()
+    s = steps(sound_duration)
+    update_progress_bar(s)
 
-def pause_sound():
-    pygame.mixer.music.pause()
+def update_progress_bar(gerador_steps):
+    global is_pause
     
+    if is_pause:
+        app.after(100, lambda: update_progress_bar(gerador_steps))
+        return
+    
+    try:
+        t = next(gerador_steps)
+        progress.set(t)
+        app.after(10, lambda: update_progress_bar(gerador_steps))
+        
+    except:
+        pass
+
 def steps(valor_original):
     total_passos = int(valor_original * 100)  # cada 0.01s é um passo
     for passo in range(total_passos + 1):
         novo_valor = (passo / total_passos)  # valor de 0.0 até 1.0
         yield novo_valor
+
+def pause_sound():
+    global is_pause
+    
+    if is_pause:
+        pygame.mixer.music.unpause()
+        is_pause = False
+        
+    else:
+        pygame.mixer.music.pause()
+        is_pause = True
+
 
 music_frame = tkinter.Frame(app, bg="#2e2e2e")
 music_frame.pack(fill="both", expand=True)
